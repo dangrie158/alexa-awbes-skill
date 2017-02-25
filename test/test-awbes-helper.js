d119d614347deffbf216a5a1b65a78896d3d0c5b
Daniel Grießhaber <dangrie158@gmail.com>
2017-02-25 12:10:50 +0100

    added helper module to crawl the data from the AWB-ES site

---
+++ test/test-awbes-helper.js
@@ -0,0 +1,93 @@
+var assert = require('assert');
+
+var testling = require('../lib/awbes-helper.js');
+
+describe('AWB-ES Helper', function() {
+  describe('#getDistrictCities', function() {
+    it(
+      'should return an empty array when searching for an unknown city',
+      function(done) {
+        testling.getDistrictCities('NoRealCity')
+          .then((data) => {
+            assert.deepEqual(data, []);
+            done();
+          })
+          .catch((err) => done(err));
+      });
+
+    it(
+      'should return the full city name when passed part of the name',
+      function(done) {
+        testling.getDistrictCities('Leinfelden')
+          .then((data) => {
+            assert.deepEqual(data, ['Leinfelden-Echterdingen']);
+            done();
+          })
+          .catch((err) => done(err));
+      });
+  });
+
+  describe('#getDistrictStreets', function() {
+    it(
+      'should return an empty array when searching for an unknown street',
+      function(done) {
+        testling.getDistrictStreets('Leinfelden-Echterdingen',
+            'NoRealStreet')
+          .then((data) => {
+            assert.deepEqual(data, []);
+            done();
+          })
+          .catch((err) => done(err));
+      });
+
+    it(
+      'should return the full street name when passed part of it',
+      function(done) {
+        testling.getDistrictStreets('Leinfelden-Echterdingen',
+            'Fuchs')
+          .then((data) => {
+            assert.deepEqual(data, ['Fuchsweg']);
+            done();
+          })
+          .catch((err) => done(err));
+      });
+  });
+
+  describe('#getDistrictDates', function() {
+    it(
+      'should get a list of dates for a known district',
+      function(done) {
+        testling.getDistrictDates('Leinfelden-Echterdingen',
+            'Fuchsweg')
+          .then((data) => {
+            assert.notEqual(data.length, 0);
+            done();
+          })
+          .catch((err) => done(err));
+      });
+
+    it(
+      'should return an empty list when filtering for an unknown waste type',
+      function(done) {
+        testling.getDistrictDates('Leinfelden-Echterdingen',
+            'Fuchsweg', 'NotATrashType')
+          .then((data) => {
+            assert.equal(data.length, 0);
+            done();
+          })
+          .catch((err) => done(err));
+      });
+
+    it(
+      'should not return an empty list when filtering for a known waste type',
+      function(done) {
+        testling.getDistrictDates('Leinfelden-Echterdingen',
+            'Fuchsweg', 'B')
+          .then((data) => {
+            assert.equal(data.length, 0);
+            done();
+          })
+          .catch((err) => done(err));
+      });
+  });
+});
